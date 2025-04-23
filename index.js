2eb72dd3234b3d1bedc09970eb53446806677d06
rodrigo <141788960+rodrigo1488@users.noreply.github.com>
2025-04-23 00:44:59 +0000

    att

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var pprova;
 var sprova;
 var snota;
@@ -57,7 +58,7 @@ function botaocalc() {
     botao.style.borderRadius = "15px";
     botao.style.cursor = "pointer";
     botao.style.boxshadow = "2px 2px 2px black";
-
+    
     botao.addEventListener("click", function novo_calculo() {
         location.reload();
     });
@@ -126,18 +127,19 @@ function aprovado(ne_conv,ver_p2){
 }
 
 
-const SUPABASE_URL = 'https://rfosgikzezrmhzghjjwd.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmb3NnaWt6ZXpybWh6Z2hqandkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzNjA5NTYsImV4cCI6MjA2MDkzNjk1Nn0.oRpEXqpBTTHLDewfkCFjR2oCr2qtzazeOvn_NGsG1w4';
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
 
 function insertData() {
+    const SUPABASE_URL = 'https://rfosgikzezrmhzghjjwd.supabase.co';
+    const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmb3NnaWt6ZXpybWh6Z2hqandkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzNjA5NTYsImV4cCI6MjA2MDkzNjk1Nn0.oRpEXqpBTTHLDewfkCFjR2oCr2qtzazeOvn_NGsG1w4';
+    const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
     const nota_p1 = pprova;
     const nota_p2 = sprova;
     const nota_n2 = snota;
     const participacao = participa;
     const materia = tema_v;
     const media_final = mf_conv;
-    const id_aluno = localStorage.getItem('id_aluno');
+    const id_aluno = localStorage.getItem('user_id');
 
     const data = {
         nota_p1: nota_p1,
@@ -157,6 +159,8 @@ function insertData() {
                 console.error('Erro ao inserir dados:', error);
             } else {
                 console.log('Dados inseridos com sucesso:', data);
+                alert('Dados inseridos com sucesso!');
+                window.location.reload(); // Recarrega a página após a inserção
             }
             // Aqui você pode adicionar lógica adicional após a inserção, se necessário
         })
