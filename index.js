1114cf7e727f1b6d6f7f318e211282307fbdb9d7
rodrigo <141788960+rodrigo1488@users.noreply.github.com>
2025-04-23 21:49:22 -0300

    att

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const materiaSelecionada = JSON.parse(localStorage.getItem('materiaSelecionada'));
 var pprova;
 var sprova;
 var snota;
@@ -10,11 +9,69 @@ var bot_calc = document.getElementById("bot_media");
 var tema_v
 var minp2
 var aprovado
+const SUPABASE_URL = 'https://rfosgikzezrmhzghjjwd.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmb3NnaWt6ZXpybWh6Z2hqandkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzNjA5NTYsImV4cCI6MjA2MDkzNjk1Nn0.oRpEXqpBTTHLDewfkCFjR2oCr2qtzazeOvn_NGsG1w4';
+
+
+
+function carregarmaterias() {
+    
+    const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+    const id_aluno = localStorage.getItem('user_id');
+    supabase
+    .from('materias')
+    .select()
+    .eq('id_aluno', id_aluno)
+    .then(({ data, error }) => {
+        if (error) {
+            console.log(error);
+        } else {
+            const materias = data;
+            const select = document.getElementById('tema1');
+            
+            materias.forEach(materia => {
+                const option = document.createElement('option');
+                option.value =  JSON.stringify({ id: materia.id, materia: materia.materia }); // guarda ambos
+                option.textContent = materia.materia;
+                select.appendChild(option);
+            });
+
+            // Adiciona o event listener após popular o select
+            select.addEventListener('change', () => {
+                const materiaSelecionada = select.value;
+                localStorage.setItem('materiaSelecionada', materiaSelecionada);
+
+            });
+        }
+    });
+
+}
+
+function Getstorage(){
+    if (localStorage.getItem('user_id') == null) {
+        alert("Você não está logado, faça o login para continuar.");
+        window.location.href = 'login.html';
+    } else {
+        const nome = localStorage.getItem('nome');
+        const email = localStorage.getItem('email');
+        document.getElementById('saudacao').innerHTML = `<p>Bem-vindo, ${nome} </p>`;
+    }
+
+        
+
+}
+
+
+
+
+
+
+
+
 
 
 
 function soma() {
-    tema_v = materiaSelecionada?.materia;
     pprova = parseFloat(document.calcmedia.p1.value);
     sprova = parseFloat(document.calcmedia.p2.value);
     snota = parseFloat(document.calcmedia.n2.value);
@@ -22,21 +79,23 @@ function soma() {
     if (isNaN(sprova) || !isFinite(sprova)) {
         sprova = 0;
     }
-    mf = (((pprova + sprova) / 2) * 6 + snota * 4) / 10 + participa;
+    mf = (((pprova + sprova) / 2) * 6 + snota * 4) / 10 + participa;//(p1+p2)/2*6 + n2*4/10 + participação
     mf_conv = mf.toFixed(1);
     
     
 }
 
 
+
 function calculop2() {
-    p1 = parseFloat(document.calcmedia.p1.value);
+    var p1 = parseFloat(document.calcmedia.p1.value);
     var n2 = parseFloat(document.calcmedia.n2.value);
     var part = parseFloat(document.calcmedia.participa1.value);
-    var nprov = ((n2 * 4) / 10) + part;
-    var nm = 6.0;
-    var ne = ((((nm - nprov) * 10) / 6) * 2) - p1+0.1;  
+    var nprov = ((n2 * 4) / 10) + part; //(n2*4/10)+participação => pega os 40% da n2 e soma com a participação
+    var ne = ((((6 - nprov) * 10) / 6) * 2) - p1+0.1;  
     var ne_conv =ne.toFixed(1);
+    localStorage.setItem('NotaNecessariaP2',ne_conv);
+
     if(ne_conv>0){
         exibe_p2(ne_conv);
     }else{
@@ -53,25 +112,10 @@ function calculop2() {
 }
 
 
-function botaocalc() {
-    var botao = document.createElement("button");
-    botao.innerHTML = "novo calculo";
-    botao.style.padding = "10px";
-    botao.style.backgroundColor = "green";
-    botao.style.color = "white";
-    botao.style.border = "2px solid white";
-    botao.style.borderRadius = "15px";
-    botao.style.cursor = "pointer";
-    botao.style.boxshadow = "2px 2px 2px black";
-    
-    botao.addEventListener("click", function novo_calculo() {
-        location.reload();
-    });
-    document.getElementById("botaoContainer").appendChild(botao);
-}
 
 
 function exibemf() {
+
     soma();
 
     if (isNaN(pprova) || isNaN(snota)) {
@@ -80,8 +124,8 @@ function exibemf() {
     } else if (mf_conv >= 6) {
         bot_calc.style.visibility = "hidden";
         var resultado = document.getElementById('resultado');
-        resultado.innerHTML = 'Sua média em ' + tema_v + ' é: ' + mf_conv + ' Parabéns! Você atingiu a média mínima.';
-        resultado.style.color = "green";
+        resultado.innerHTML = 'Sua média em ' + JSON.parse(localStorage.getItem('materiaSelecionada')).materia + ' é: ' + mf_conv + ' Parabéns! Você atingiu a média mínima.';
+        resultado.style.color = "white";
 
         
         //botaocalc();
@@ -90,8 +134,8 @@ function exibemf() {
         bot_calc.style.visibility = "hidden";
         var falta = 6 - mf_conv;
         var resultado = document.getElementById('resultado');
-        resultado.innerHTML = 'Sua média em ' + tema_v + ' é: ' + mf_conv + ' Você precisa de ' + falta.toFixed(2) + ' para atingir a média necessária.';
-        resultado.style.color = "red";
+        resultado.innerHTML = 'Sua média em ' + JSON.parse(localStorage.getItem('materiaSelecionada')).materia+ ' é: ' + mf_conv + ' Você precisa de ' + falta.toFixed(2) + ' para atingir a média necessária.';
+        resultado.style.color = "white";
         vp2()
        // botaocalc();
         mostrarBotaoSalvar(); // Chama a função para mostrar o botão de salv
@@ -123,43 +167,42 @@ function exibe_p2(ne_conv,ver_p2){
     var resultado2 = document.getElementById('resultado2');
         resultado.innerHTML = 'você precisa tirar '+ne_conv+' pontos na p2 para atingir a media final';
 
-        resultado.style.color = "red";
+        resultado.style.color = "white";
         botao_p2.style.display = 'none';
 }
 
 function aprovado(ne_conv,ver_p2){
     var botao_p2 =document.getElementById('ver_p2')
     var resultado2 = document.getElementById('resultado2');
-        resultado.innerHTML = 'Parabens! Voce foi aprovado em '+tema_v+' com a media de '+mf_conv+' pontos';
-        resultado.style.color = "green";
+        resultado.innerHTML = 'Parabens! Voce foi aprovado em '+JSON.parse(localStorage.getItem('materiaSelecionada')).materia+' com a media de '+mf_conv+' pontos';
+        resultado.style.color = "white";
         botao_p2.style.display = 'none';
 }
 
 
 
 
-function insertData(ne_conv) {
-    const SUPABASE_URL = 'https://rfosgikzezrmhzghjjwd.supabase.co';
-    const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmb3NnaWt6ZXpybWh6Z2hqandkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzNjA5NTYsImV4cCI6MjA2MDkzNjk1Nn0.oRpEXqpBTTHLDewfkCFjR2oCr2qtzazeOvn_NGsG1w4';
-    
-    const id_materia = materiaSelecionada?.id;
+function insertData() {
+    const id_materia = JSON.parse(localStorage.getItem('materiaSelecionada')).id;  // Obtém o ID da matéria selecionada
+    const materia = JSON.parse(localStorage.getItem('materiaSelecionada')).materia; // Obtém o nome da matéria selecionada
     const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+    const nota_p_p2= JSON.parse(localStorage.getItem('NotaNecessariaP2'));
     const nota_p1 = pprova;
     const nota_p2 = sprova;
     const nota_n2 = snota;
     const participacao = participa;
-    const materia = tema_v;
     const media_final = mf_conv;
     const id_aluno = localStorage.getItem('user_id');
     const status_aprovado = (media_final >= 6) ? 'aprovado' : 'reprovado'; // Define o status de aprovação com base na média final
     
 
     const data = {
+        nota_p_p2: nota_p_p2,
         nota_p1: nota_p1,
         nota_p2: nota_p2,
         nota_n2: nota_n2,
-        participacao: participacao,
         materia: materia,
+        participacao: participacao,
         media_final: media_final,
         id_aluno: id_aluno,
         id: id_materia,
@@ -177,6 +220,7 @@ function insertData(ne_conv) {
             } else {
                 
                 localStorage.removeItem('materiaSelecionada');
+                localStorage.removeItem('NotaNecessariaP2'); // Remove o item do localStorage após a inserção
                 alert('Dados inseridos com sucesso!');
 
                 location.reload(); // Recarrega a página após a inserção
@@ -187,6 +231,11 @@ function insertData(ne_conv) {
 
 
 
+
+
+
 }
 
 
+Getstorage();
+carregarmaterias();
